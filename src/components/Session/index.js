772e6b42b7ee1d1b82c93287b59a7058f8d600a1
Danil Khrustov <khrustovd@gmail.com>
2019-05-02 11:07:04 -0500

    Working on Auth and Unauth users

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,12 +1,5 @@
-import React from "react";
-import AuthUserContext from "./context";
-import withAuthentication from "./withAuthentication";
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
 
-const Session = () => (
-  <div>
-    <h1>Session</h1>
-  </div>
-);
-
-export default Session;
-export { AuthUserContext, withAuthentication };
+export { AuthUserContext, withAuthentication, withAuthorization };
