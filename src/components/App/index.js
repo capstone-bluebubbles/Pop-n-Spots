e100ec0acd089844cde817c7c13d083500f1baaa
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-05-05 18:26:53 -0500

    Added Store

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { withFirebase } from "../Firebase";
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -9,24 +9,21 @@ import PasswordForgetPage from "../PasswordForget";
 import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
-
 import * as ROUTES from "../../constants/routes";
 
 const App = () => (
-  <Router>
+  <div>
     <Navigation />
     <hr />
-    <div>
-      <Route exact path={ROUTES.DEFAULT} component={HomePage} />
-      <Route path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
+    <Route exact path={ROUTES.DEFAULT} component={HomePage} />
+    <Route path={ROUTES.LANDING} component={LandingPage} />
+    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+    <Route path={ROUTES.HOME} component={HomePage} />
+    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+    <Route path={ROUTES.ADMIN} component={AdminPage} />
+  </div>
 );
 
 export default withFirebase(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-
-import App from './components/App';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../src/store/index";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./components/App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-      <App />
-    </FirebaseContext.Provider>,
-    document.getElementById('root'),
-  );
+  <Provider store={store}>
+    <Router>
+      <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+      </FirebaseContext.Provider>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
