54e9ba70a94cc933c69d2f99f031823e76851c33
Danil Khrustov <khrustovd@gmail.com>
2019-05-07 10:08:14 -0500

    Firebase working and populates redux with all data. Cleaned up the firebase file.

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    const snapshot = await admin.database().ref('/messages').push({original: original});
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref.toString());
+  });
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
