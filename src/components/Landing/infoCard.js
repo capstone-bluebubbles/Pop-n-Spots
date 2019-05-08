8b318f137bc88344d4393a1000d785bfc98689f3
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-05-07 22:47:05 -0500

    completed basic card structure

--- src/components/Landing/infoCard.js
+++ src/components/Landing/infoCard.js
@@ -2,12 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 
 const InfoCard = props => {
-  console.log(`what is this thing?????`, this);
+  //console.log(`THE OBJECT===>`, props);
+  const weekObject = props.place.popularTimesHistogram;
+
+  for (let key in weekObject) {
+    console.log(`!!!!`, key);
+  }
+
+  console.log(`This THINGG-----<`, weekObject);
   return (
     <div className="info-container">
       <h2>{props.place.title}</h2>
       <ul>
-        <li>{props.place.address}</li>
+        <li>Address: {props.place.address}</li>
+        <li>Phone: {props.place.phone}</li>
+        <li>Star Rating: {props.place.totalScore}</li>
+        {/* {props.place.popularTimesHistogram.Fr.map((time, index) => (
+          <li key={index}>{time}</li>
+        ))} */}
       </ul>
     </div>
   );
