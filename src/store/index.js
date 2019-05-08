750ab0935719dd0ea9941d68478a35639a512f2f
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-05-08 16:40:05 -0500

    fixedstore

--- src/store/index.js
+++ src/store/index.js
@@ -4,24 +4,17 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { placesReducer } from "./places";
-<<<<<<< HEAD
 import { userReducer } from "./user";
-=======
-import { positionReducer } from "./position"
->>>>>>> ec793881c0f1568162f64fb5dda885febcc7e776
+import { positionReducer } from "./position";
 
 // store imports
 //import userReducer from "../store/user";
 
 const reducer = combineReducers({
-<<<<<<< HEAD
   //user: userReducer,
-  places: placesReducer
-=======
-  //user: userReducer
-  position: positionReducer,
   places: placesReducer,
->>>>>>> ec793881c0f1568162f64fb5dda885febcc7e776
+  //user: userReducer
+  position: positionReducer
 });
 
 const middleware = composeWithDevTools(
