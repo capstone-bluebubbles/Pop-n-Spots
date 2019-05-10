d4fbc9fca093fac9bed647d8ead25eaea4505772
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-05-09 22:32:38 -0500

    Hamburger menue Finished

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -50,20 +50,22 @@ class NavigationAuth extends React.Component {
           </li>
         </ul>
         <ul className="nav-flex-burger">
-          <Link
-            to={"javascript:void(0)"}
-            className="icon"
-            onClick={this.toggleOpen}>
+          <Link className="icon" onClick={this.showMenu}>
             <i className="fa fa-bars" />
           </Link>
+          {/* Hamburger Drop Down Menue */}
           <div>
-            <button onClick={this.showMenu}>Show menu</button>
-
             {this.state.showMenu ? (
               <div className="menu">
-                <button> Menu item 1 </button>
-                <button> Menu item 2 </button>
-                <button> Menu item 3 </button>
+                <li>
+                  <Link to={ROUTES.LANDING}>Map</Link>
+                </li>
+                <li>
+                  <Link to={ROUTES.ACCOUNT}>POPS!</Link>
+                </li>
+                <li>
+                  <SignOutButton />
+                </li>
               </div>
             ) : null}
           </div>
@@ -73,24 +75,22 @@ class NavigationAuth extends React.Component {
   }
 }
 
-//HAMBURGER MENU TAKEN OUT FOR NOW. -JH
-{
-  /* <div className={this.state.open ? "open" : "closed"}>
-
-</div> */
-}
-
 class NavigationNonAuth extends React.Component {
   constructor() {
     super();
-    this.state = { open: true };
-    this.toggleOpen = this.toggleOpen.bind(this);
+    this.state = { showMenu: false };
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
   }
-  toggleOpen() {
-    this.setState(state => {
-      return {
-        open: !state.open
-      };
+  showMenu() {
+    // event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  }
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener("click", this.closeMenu);
     });
   }
   render() {
@@ -108,33 +108,31 @@ class NavigationNonAuth extends React.Component {
           <li>
             <Link to={ROUTES.ACCOUNT}>POPS!</Link>
           </li>
+          <li>
+            <SignOutButton />
+          </li>
         </ul>
         <ul className="nav-flex-burger">
-          <Link
-            to={"javascript:void(0)"}
-            className="icon"
-            onClick={this.toggleOpen}>
+          <Link className="icon" onClick={this.showMenu}>
             <i className="fa fa-bars" />
           </Link>
+          {/* Hamburger Drop Down Menue */}
+          <div>
+            {this.state.showMenu ? (
+              <div className="menu">
+                <li>
+                  <Link to={ROUTES.LANDING}>Map</Link>
+                </li>
+                <li>
+                  <Link to={ROUTES.ACCOUNT}>POPS!</Link>
+                </li>
+              </div>
+            ) : null}
+          </div>
         </ul>
       </div>
     );
   }
 }
-
-//HAMBURGER MENU
-// this.setState(state => {
-//   return {
-//     open: !state.open
-//   };
-// });
-
-/* <li>
-<div className={this.state.open ? "open" : "closed"}>
-  <Link to={""} className="icon" onClick={this.toggleOpen}>
-    <i className="fa fa-bars" />
-  </Link>
-</div>
-</li> */
 
 export default Navigation;
