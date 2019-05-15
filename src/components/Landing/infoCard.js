ccb171e0af4da89b7140e647402bbb3fa95b709e
Danil Khrustov <khrustovd@gmail.com>
2019-05-14 21:29:12 -0500

    reworked infocards. phone number doesnt work. miles missing

--- src/components/Landing/infoCard.js
+++ src/components/Landing/infoCard.js
@@ -1,76 +1,78 @@
-import React from "react";
-import { connect } from "react-redux";
-import { fetchUser, fetchPops } from "../../store/user";
-import { Link, Redirect} from "react-router-dom";
-import * as ROUTES from "../../constants/routes";
-import { AuthUserContext, withAuthorization } from "../Session";
-import {userRef} from '../Firebase/firebase'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser, fetchPops } from '../../store/user';
+import { Link, Redirect } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import { AuthUserContext, withAuthorization } from '../Session';
+import { userRef } from '../Firebase/firebase';
 
 class InfoCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pops: []
+      pops: [],
     };
     this.handleClick = this.handleClick.bind(this);
 
     // current day abbreviation
     const date = new Date();
-    const days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+    const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
     this.currentDay = days[date.getDay()];
     this.currentHour = date.getHours();
-    this.bubbles=['NewBubble00.png', 'NewBubble33.png', 'NewBubble66.png', 'NewBubble99.png']
+    this.bubbles = [
+      'NewBubble00.png',
+      'NewBubble33.png',
+      'NewBubble66.png',
+      'NewBubble99.png',
+    ];
   }
 
-    //Pop function
+  //Pop function
   async addPop(uID, locationId) {
     try {
       const User = userRef.child(uID);
-      const popsRef = User.child('pops')
+      const popsRef = User.child('pops');
       await popsRef.on('value', snapshot => {
         const pops = snapshot.val();
         this.setState({
-          pops: pops
-        })
-      })
-      let pops = await this.state.pops
-      let popFound = false
+          pops: pops,
+        });
+      });
+      let pops = await this.state.pops;
+      let popFound = false;
       let placeIndex;
       let timesPopped;
       for (let i = 0; i <= pops.length - 1; i++) {
         if (pops[i].placeKey === locationId) {
-          popFound = true
-          placeIndex = i.toString()
-          timesPopped = this.state.pops[i].timestamp.length
+          popFound = true;
+          placeIndex = i.toString();
+          timesPopped = this.state.pops[i].timestamp.length;
           break;
         }
       }
       if (popFound === true) {
-        let foundPlaceRef = popsRef.child(`${placeIndex}`)
+        let foundPlaceRef = popsRef.child(`${placeIndex}`);
         foundPlaceRef.update({
-          'dropped': false
-        })
-        let popRef = foundPlaceRef.child('timestamp').child(`${timesPopped}`)
+          dropped: false,
+        });
+        let popRef = foundPlaceRef.child('timestamp').child(`${timesPopped}`);
 
-        popRef.set(
-          Date.now()
-        )
+        popRef.set(Date.now());
       } else {
-        const length = pops.length.toString()
-        let popper = popsRef.child(`${length}`)
+        const length = pops.length.toString();
+        let popper = popsRef.child(`${length}`);
         popper.update({
-          'popIndex': length,
-          'placeKey': locationId,
-          'timestamp': {
-            0: Date.now()
-          }
-        })
+          popIndex: length,
+          placeKey: locationId,
+          timestamp: {
+            0: Date.now(),
+          },
+        });
       }
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
-
 
   historyData = popData => {
     let popDataTarget = 0;
@@ -108,131 +110,139 @@ class InfoCard extends React.Component {
 
     // safety check
     popDataTargetFrame = Math.max(popDataTargetFrame, 0);
-    popDataTargetFrame = Math.min(
-      popDataTargetFrame,
-      this.bubbles.length - 1
-    );
+    popDataTargetFrame = Math.min(popDataTargetFrame, this.bubbles.length - 1);
 
-    return(
-      <div>
-        <div> {popDataTarget}% POPPIN</div>
-        <img src = {`${this.bubbles[popDataTargetFrame]}`} />
+    return (
+      <div className="pops-card-title-icon-container">
+        <div> {`${popDataTarget}% Poppin`} </div>
+        <img
+          className="pops-card-title_icon"
+          style={{ height: '30px', width: '30px' }}
+          src={`${this.bubbles[popDataTargetFrame]}`}
+        />
       </div>
-      
-    ) 
+    );
   };
 
   handleClick(location, address) {
-    window.open(`http://maps.google.com/maps?saddr=${this.props.currentPosition.lat}+${this.props.currentPosition.lng}&daddr=${location},${address}`);
+    window.open(
+      `http://maps.google.com/maps?saddr=${this.props.currentPosition.lat}+${
+        this.props.currentPosition.lng
+      }&daddr=${location},${address}`
+    );
   }
 
   render() {
-      let popDataTarget = 0;
-      const popData = this.props.place.popularTimesHistogram;
-      if (popData) {
-        const popDataCurrentDay = popData[this.currentDay];
-        if (popDataCurrentDay) {
-          for (let index = 0; index < popDataCurrentDay.length; index++) {
-            const hour = popDataCurrentDay[index].hour;
-            if (hour === this.currentHour) {
-              popDataTarget = popDataCurrentDay[index].occupancyPercent;
-            }
+    let popDataTarget = 0;
+    const popData = this.props.place.popularTimesHistogram;
+    if (popData) {
+      const popDataCurrentDay = popData[this.currentDay];
+      if (popDataCurrentDay) {
+        for (let index = 0; index < popDataCurrentDay.length; index++) {
+          const hour = popDataCurrentDay[index].hour;
+          if (hour === this.currentHour) {
+            popDataTarget = popDataCurrentDay[index].occupancyPercent;
           }
         }
       }
-    return (
+    }
 
+    let place = this.props.place;
+
+    return (
       <AuthUserContext.Consumer>
-        {authUser => (authUser ? (
-          <div className="info-container">
-            <ul className="place-box">
-              <h3 className="place-title">{this.props.place.title}</h3>
-              <div className="place-title">
-                {Array.from({ length: this.props.place.totalScore }).map(
-                  (j, i) => (
-                    <span key={i}> ⭐ </span>
-                  )
-                )}
+        {authUser =>
+          authUser ? (
+            <div className="map-page-card">
+              <div className="map-card-title-container">
+                <div className="map-card-title">{place.title}</div>
+                <div className="map-card-title-icon">
+                  {this.historyData(place.popularTimesHistogram)}
+                </div>
               </div>
-            </ul>
-            <ul className="info-card">
-              <li className="place-website">
-                <Link target="_blank" onClick={()=>window.open(`https://www.${this.props.place.website}`)}>
-                  {this.props.place.website}
+              <ul className="map-card-address-container">
+                <br />
+                <div className="map-card-address">{place.address}</div>
+                {/* <div className="map-card-phone">
+                      {`${place.phone.slice(2, 5)}-${place.phone.slice(
+                        5,
+                        8
+                      )}-${place.phone.slice(8)}`}
+                    </div> */}
+                <div className="pops-card-mile">1.5 Miles</div>
+                <div className="place-title">
+                  {Array.from({ length: place.totalScore }).map((j, i) => (
+                    <span key={i}> ⭐ </span>
+                  ))}
+                </div>
+              </ul>
+              <div className="map-card-buttons-container">
+                <button
+                  className="map-card-button"
+                  type="button"
+                  onClick={() => {
+                    this.handleClick(place.title, place.address);
+                  }}
+                >
+                  NAV
+                </button>
+                <button
+                  className="pop-button"
+                  type="button"
+                  onClick={() => {
+                    this.addPop(authUser.uid, this.props.place.locationId);
+                  }}
+                >
+                  POP
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="map-page-card">
+              <div className="map-card-title-container">
+                <div className="map-card-title">{place.title}</div>
+                <div className="map-card-title-icon">
+                  {this.historyData(place.popularTimesHistogram)}
+                </div>
+              </div>
+              <ul className="map-card-address-container">
+                <br />
+                <div className="map-card-address">{place.address}</div>
+                {/* <div className="map-card-phone">
+                      {`${place.phone.slice(2, 5)}-${place.phone.slice(
+                        5,
+                        8
+                      )}-${place.phone.slice(8)}`}
+                    </div> */}
+                <div className="map-card-mile">1.5 Miles</div>
+                <div className="place-title">
+                  {Array.from({ length: place.totalScore }).map((j, i) => (
+                    <span key={i}> ⭐ </span>
+                  ))}
+                </div>
+              </ul>
+              <div className="map-card-buttons-container">
+                <button
+                  className="map-card-button"
+                  type="button"
+                  onClick={() => {
+                    this.handleClick(place.title, place.address);
+                  }}
+                >
+                  NAV
+                </button>
+
+                <Link to={ROUTES.SIGN_IN} className="pop-button" type="button">
+                  POP
                 </Link>
-              </li>
-              <br />
-              <li className="place-address">{this.props.place.address}</li>
-              <li className="phone">{this.props.place.phone}</li>
-              <br />
-              <li className="poppin-title">{popDataTarget}% POPPIN</li>
-              <div className="popular-time-percent">
-                      {' '}
-                      {this.historyData(this.props.place.popularTimesHistogram)}{' '}
-                    </div>
-            </ul>
-            <button
-              className="navigate-button"
-              type="button"
-              onClick={() => {
-                this.handleClick(this.props.place.title);
-              }}>
-              NAV
-            </button>
-            <button
-              className="pop-button"
-              type="button"
-              onClick={() => {
-                this.addPop(authUser.uid, this.props.place.locationId);
-              }}>
-              POP
-            </button>
-          </div>
-        ): (
-        <div className="info-container">
-        <ul className="place-box">
-          <h3 className="place-title">{this.props.place.title}</h3>
-          <div className="place-title">
-            {Array.from({ length: this.props.place.totalScore }).map(
-              (j, i) => (
-                <span key={i}> ⭐ </span>
-              )
-            )}
-          </div>
-        </ul>
-        <ul className="info-card">
-          <li className="place-website">
-            <Link target="_blank" to={`www.${this.props.place.website}`}>
-              {this.props.place.website}
-            </Link>
-          </li>
-          <br />
-          <li className="place-address">{this.props.place.address}</li>
-          <li className="phone">{this.props.place.phone}</li>
-          <br />
-          <li className="poppin-title">{popDataTarget}% POPPIN</li>
-        </ul>
-        <button
-          className="navigate-button"
-          type="button"
-          onClick={() => {
-            this.handleClick(this.props.place.title);
-          }}>
-          NAV
-        </button>
-        <Link to={ROUTES.SIGN_IN}
-          className="pop-button"
-          type="button">
-          POP
-        </Link>
-      </div>
-        ))
+              </div>
+            </div>
+          )
         }
       </AuthUserContext.Consumer>
-    )
-  };
+    );
+  }
 }
-
 
 const mapDispatchToProps = dispatch => ({
   fetchUser: uID => dispatch(fetchUser(uID)),
@@ -253,4 +263,4 @@ const Infocard = connect(
   mapDispatchToProps
 )(InfoCard);
 
-export default (Infocard);
+export default Infocard;
